9fca020b3268fbcd086999108b9d8d6acc8d8fb3
Lin Jian <ljian3377@gmail.com>
2020-06-15 23:39:40 +0800

    me

--- cors.ts
+++ cors.ts
@@ -1,53 +1,58 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { ShareServiceClient, StorageSharedKeyCredential } from "@azure/storage-file-share";
+import {
+  BlobServiceClient,
+  StorageSharedKeyCredential,
+} from "@azure/storage-blob";
 
 async function main() {
-    const account = process.env.ACCOUNT_NAME || "";
-    const accountKey = process.env.ACCOUNT_KEY || "";
-    console.log("using account:", account, accountKey);
-    const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
-    const serviceClient = new ShareServiceClient(
-        // When using AnonymousCredential, following url should include a valid SAS
-        // `https://${account}.queue.core.windows.net`,
-        // `https://${account}.file.core.windows.net`,
-        `https://${account}.blob.preprod.core.windows.net`,
-        // `http://127.0.0.1:10001/${account}`,
-        sharedKeyCredential
-    );
+  const account = process.env.ACCOUNT_NAME || "";
+  const accountKey = process.env.ACCOUNT_KEY || "";
+  console.log("using account:", account, accountKey);
+  const sharedKeyCredential = new StorageSharedKeyCredential(
+    account,
+    accountKey
+  );
+  const serviceClient = new BlobServiceClient(
+    // `https://${account}.queue.core.windows.net`,
+    // `https://${account}.file.core.windows.net`,
+    `https://${account}.blob.core.windows.net`,
+    // `http://127.0.0.1:10001/${account}`,
+    sharedKeyCredential
+  );
 
-    // set CORS
-    const serviceProperties = await serviceClient.getProperties();
+  // set CORS
+  const serviceProperties = await serviceClient.getProperties();
 
-    const newCORS = {
-        allowedHeaders: "*",
-        allowedMethods: "DELETE,GET,HEAD,MERGE,POST,OPTIONS,PUT",
-        allowedOrigins: "*",
-        exposedHeaders: "*",
-        maxAgeInSeconds: 86400
-    };
+  const newCORS = {
+    allowedHeaders: "*",
+    allowedMethods: "DELETE,GET,HEAD,MERGE,POST,OPTIONS,PUT",
+    allowedOrigins: "*",
+    exposedHeaders: "*",
+    maxAgeInSeconds: 86400,
+  };
 
-    if (!serviceProperties.cors) {
-        serviceProperties.cors = [newCORS];
-    } else {
-        console.log("original cors:");
-        console.log(serviceProperties.cors);
-        if (serviceProperties.cors!.length < 5) {
-            serviceProperties.cors.push(newCORS);
-        }
+  if (!serviceProperties.cors) {
+    serviceProperties.cors = [newCORS];
+  } else {
+    console.log("original cors:");
+    console.log(serviceProperties.cors);
+    if (serviceProperties.cors!.length < 5) {
+      serviceProperties.cors.push(newCORS);
     }
-    await serviceClient.setProperties(serviceProperties);
-    
-    const newServiceProperties = await serviceClient.getProperties();
-    console.log("new cors:");
-    console.log(newServiceProperties.cors);
+  }
+  await serviceClient.setProperties(serviceProperties);
+
+  const newServiceProperties = await serviceClient.getProperties();
+  console.log("new cors:");
+  console.log(newServiceProperties.cors);
 }
 
 main()
-    .then(() => {
-        console.log("Done.");
-    })
-    .catch((err) => {
-        console.log(err.message);
-    });
+  .then(() => {
+    console.log("Done.");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
