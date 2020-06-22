8d3da3d03bb8b23d3aa713c89c5c776d6cc2a26f
Lin Jian <ljian3377@gmail.com>
2020-06-22 14:11:04 +0800

    add deleteAllBlobs

--- deleteAllBlobs.ts
+++ deleteAllBlobs.ts
@@ -1,26 +1,28 @@
+import {
+  StorageSharedKeyCredential,
+  BlobServiceClient,
+} from "@azure/storage-blob";
+
 import * as dotenv from "dotenv";
 console.log(dotenv.config());
-
-import {
-  ShareServiceClient,
-  StorageSharedKeyCredential,
-} from "@azure/storage-file-share";
 
 async function main() {
   const account = process.env.ACCOUNT_NAME || "";
   const accountKey = process.env.ACCOUNT_KEY || "";
+
   const sharedKeyCredential = new StorageSharedKeyCredential(
     account,
     accountKey
   );
-  const serviceClient = new ShareServiceClient(
-    `https://${account}.file.core.windows.net`,
+
+  const serviceClient = new BlobServiceClient(
+    `https://${account}.blob.core.windows.net`,
     sharedKeyCredential
   );
 
-  for await (const item of serviceClient.listShares()) {
-    const shareClient = serviceClient.getShareClient(item.name);
-    await shareClient.delete();
+  for await (const item of serviceClient.listContainers()) {
+    const containerClient = serviceClient.getContainerClient(item.name);
+    await containerClient.delete();
   }
 }
 
