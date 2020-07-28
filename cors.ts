c5da9124e1526e4451a54a3dda76bfbfd47c4de7
Lin Jian <ljian3377@gmail.com>
2020-07-28 11:55:58 +0800

    cors for blob, file and quue

--- cors.ts
+++ cors.ts
@@ -1,35 +1,30 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// import {
-//   BlobServiceClient,
-//   StorageSharedKeyCredential,
-// } from "@azure/storage-blob";
+import {
+  BlobServiceClient,
+  StorageSharedKeyCredential as BlobStorageSharedKeyCredential,
+} from "@azure/storage-blob";
 
 import {
   ShareServiceClient,
-  StorageSharedKeyCredential,
+  StorageSharedKeyCredential as FileStorageSharedKeyCredential,
 } from "@azure/storage-file-share";
 
-async function main() {
-  const account = process.env.ACCOUNT_NAME || "";
-  const accountKey = process.env.ACCOUNT_KEY || "";
-  console.log("using account:", account, accountKey);
-  const sharedKeyCredential = new StorageSharedKeyCredential(
-    account,
-    accountKey
-  );
-  // const serviceClient = new BlobServiceClient(
-  const serviceClient = new ShareServiceClient(
-    // `https://${account}.queue.core.windows.net`,
-    `https://${account}.file.core.windows.net`,
-    // `https://${account}.blob.core.windows.net`,
-    // `http://127.0.0.1:10001/${account}`,
-    sharedKeyCredential
-  );
+import {
+  QueueServiceClient,
+  StorageSharedKeyCredential as QueueStorageSharedKeyCredential,
+} from "@azure/storage-queue";
 
-  // set CORS
+async function setCors(
+  serviceClient: BlobServiceClient | ShareServiceClient | QueueServiceClient
+) {
   const serviceProperties = await serviceClient.getProperties();
+  if (serviceProperties.cors) {
+    console.log("original cors:");
+    console.log(serviceProperties.cors);
+    return;
+  }
 
   const newCORS = {
     allowedHeaders: "*",
@@ -38,21 +33,51 @@ async function main() {
     exposedHeaders: "*",
     maxAgeInSeconds: 86400,
   };
-
-  if (!serviceProperties.cors) {
-    serviceProperties.cors = [newCORS];
-  } else {
-    console.log("original cors:");
-    console.log(serviceProperties.cors);
-    if (serviceProperties.cors!.length < 5) {
-      serviceProperties.cors.push(newCORS);
-    }
-  }
-  await serviceClient.setProperties(serviceProperties);
+  serviceProperties.cors = [newCORS];
+  await serviceClient.setProperties(serviceProperties as any);
 
   const newServiceProperties = await serviceClient.getProperties();
   console.log("new cors:");
   console.log(newServiceProperties.cors);
+}
+
+async function main() {
+  const account = process.env.ACCOUNT_NAME || "";
+  const accountKey = process.env.ACCOUNT_KEY || "";
+  console.log("using account:", account, accountKey);
+
+  console.log("Setting CORS for Blob service.");
+  const blobSharedKeyCredential = new BlobStorageSharedKeyCredential(
+    account,
+    accountKey
+  );
+  const blobServiceClient = new BlobServiceClient(
+    `https://${account}.blob.core.windows.net`,
+    blobSharedKeyCredential
+  );
+  await setCors(blobServiceClient);
+
+  console.log("Setting CORS for File service.");
+  const fileSharedKeyCredential = new FileStorageSharedKeyCredential(
+    account,
+    accountKey
+  );
+  const shareServiceClient = new ShareServiceClient(
+    `https://${account}.file.core.windows.net`,
+    fileSharedKeyCredential
+  );
+  await setCors(shareServiceClient);
+
+  console.log("Setting CORS for Queue service.");
+  const queueSharedKeyCredential = new QueueStorageSharedKeyCredential(
+    account,
+    accountKey
+  );
+  const queueServiceClient = new QueueServiceClient(
+    `https://${account}.queue.core.windows.net`,
+    queueSharedKeyCredential
+  );
+  await setCors(queueServiceClient);
 }
 
 main()
