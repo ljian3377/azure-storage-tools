14a736e7ea05d62cc25dacd3f0c44eab6abde123
Lin Jian <ljian3377@gmail.com>
2020-08-18 18:28:29 +0800

    add script

---
+++ largeScale/vrfs.sh
@@ -0,0 +1,20 @@
+screen
+
+# build vrfs
+sudo apt-get update
+sudo apt-get install -y fuse3 libfuse3-dev build-essential cmake
+
+# clone code and build
+git clone https://github.com/JinmingHu-MSFT/Virtual-Random-File.git
+cd Virtual-Random-File/
+mkdir build
+cd build/
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+
+# run binary
+mkdir mnt
+./vrfs $1 mnt &
+
+# test read speed
+dd bs=16K count=$((65536*10)) if=mnt/testfile of=/dev/null
