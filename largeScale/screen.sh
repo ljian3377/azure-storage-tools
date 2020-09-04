bc64115250d532a755ae9ba62ef99897e0c7e16f
Lin Jian <ljian3377@gmail.com>
2020-09-04 20:23:06 +0800

    wip

--- largeScale/screen.sh
+++ largeScale/screen.sh
@@ -3,5 +3,5 @@
 range=50000*4000*1024*1024/8;
 for i in $(seq 0 7); 
 do
-    if [ -z "$STY" ]; then exec screen -dm -S `download$i` ts-node verifyRange $range*$i $range*($i+1); fi
+    if [ -z "$STY" ]; then exec screen -dm -S "download$i" ts-node verifyRange $range*$i $range*$(($i+1)); fi
 done
