703bcb25848eaaa5266fea84753cb9606f6e4d8d
Lin Jian <ljian3377@gmail.com>
2020-09-04 21:36:37 +0800

    wip

--- largeScale/screen.sh
+++ largeScale/screen.sh
@@ -3,5 +3,5 @@
 range=50000*4000*1024*1024/8;
 for i in $(seq 0 7); 
 do
-    screen -dmS "download$i" ts-node verifyRanges.ts $range*$i $range*$(($i+1)) > ./out$i
+    screen -dmS "download$i" bash -c "ts-node verifyRanges.ts $range*$i $range*$(($i+1)) > ./out$i"
 done
