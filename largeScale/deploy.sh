7247fb4e4ffb0b7301106cc11ee80699d6a54dea
Lin Jian <ljian3377@gmail.com>
2020-09-07 19:10:05 +0800

    wip

--- largeScale/deploy.sh
+++ largeScale/deploy.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 # git clone https://github.com/ljian3377/azure-storage-tools.git
+git clone https://github.com/Azure/azure-sdk-for-js.git
 
 # install nvm
 curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
-source ~/.bashrc
+source ~/.nvm/nvm.sh
 
 # install and set specific version of node.js as default
-nvm install v12.18.0 && nvm alias default v12.18.0 && npm install -g @types/node ts-node typescript && npm install
+nvm install v12.18.0 && nvm alias default v12.18.0 && npm install -g @types/node ts-node typescript && npm install -g @microsoft/rush
+source ~/.bashrc
