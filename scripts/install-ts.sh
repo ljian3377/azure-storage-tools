5cb7fc66ff0be2337a94c2ce77266906dff48e47
Lin Jian <lijian2@microsoft.com>
2020-10-27 15:48:10 +0800

    me

--- scripts/install-ts.sh
+++ scripts/install-ts.sh
@@ -8,5 +8,5 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
 source ~/.nvm/nvm.sh
 
 # install and set specific version of node.js as default
-nvm install v12.18.0 && nvm alias default v12.18.0 && npm install -g @types/node ts-node typescript && npm install -g @microsoft/rush
+nvm install v12.18.0 && nvm alias default v12.18.0 && npm install -g @microsoft/rush @types/node autorest ts-node typescript
 source ~/.bashrc
